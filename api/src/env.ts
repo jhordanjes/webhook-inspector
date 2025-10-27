b29fb7677fb65c2b30e1ec9e61a32c79c6148ef3
jhordan.ja <jhordan.ja@sidi.org.br>
2025-10-27 11:11:45 -0400

    feat: create api structure with docker, drizzle and fastify

---
+++ api/src/env.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  NOVE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+  DATABASE_URL: z.url(),
+})
+
+export const env = envSchema.parse(process.env)
